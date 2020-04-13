b6b497678700b7e16d92124d38dec7e7761fb1a5
Jimmy <jimmy123good@hotmail.com>
2020-04-13 15:02:31 -0700

    change default base url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const apiBaseUrl =
   ((local: Boolean) => local ?
     'http://127.0.0.1:5000/' :
-    'http://hvaccloud.org:8080/')(true);
+    'http://hvaccloud.org:5000/')(true);
 
 export { apiBaseUrl };
